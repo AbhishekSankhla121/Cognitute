9417b9b54ce963f44ed5eb0f2d0f55d751d6a9ad
abhihek sankhla <abhiheksankhla121@gmail.com>
2025-08-29 12:35:05 +0000

    setup google auth and basic get endpoint for testing

--- application/src/app/api/v1/getalldata/route.ts
+++ application/src/app/api/v1/getalldata/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
+import prisma from "../../../../../lib/prisma"
 export async function GET(
  req: NextRequest,res:NextResponse
 
 ) {
-  
+  const data = await prisma.user.findFirst()
+  console.log(data)
   return NextResponse.json({"abhishek":"asas"});
 }
